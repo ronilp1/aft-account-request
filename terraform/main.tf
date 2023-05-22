fd5802ed55019c094e10705a665818195716b3ab
rrprasad <rrprasad@amazon.com>
2023-05-23 10:24:39 +1200

    add account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,32 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "rrprasad+aftdocs-sandbox-04@amazon.com"
+    AccountName               = "sandbox-aft-04"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "rrprasad+aftdocs-sandbox-04@amazon.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}

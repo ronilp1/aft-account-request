86274197ea3cf029be42f51dc9cb48ff1884c222
rrprasad <rrprasad@amazon.com>
2023-05-24 11:44:05 +1200

    change account name

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ module "sandbox" {
 
   control_tower_parameters = {
     AccountEmail              = "rrprasad+aftdocs-sandbox@amazon.com"
-    AccountName               = "sandbox-aft-06"
+    AccountName               = "sandbox-aft-03"
     ManagedOrganizationalUnit = "Sandbox"
     SSOUserEmail              = "rrprasad+aftdocs-sandbox@amazon.com"
     SSOUserFirstName          = "Sandbox"
@@ -27,30 +27,30 @@ module "sandbox" {
 }
 
 
-module "sandbox-02" {
-  source = "./modules/aft-account-request"
+# module "sandbox" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "rrprasad+aftdocs-sandbox-04@amazon.com"
-    AccountName               = "sandbox-aft-04"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "rrprasad+aftdocs-sandbox-04@amazon.com"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "rrprasad+aftdocs-sandbox-04@amazon.com"
+#     AccountName               = "sandbox-aft-04"
+#     ManagedOrganizationalUnit = "Sandbox"
+#     SSOUserEmail              = "rrprasad+aftdocs-sandbox-04@amazon.com"
+#     SSOUserFirstName          = "Sandbox"
+#     SSOUserLastName           = "AFT"
+#   }
 
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
+#   account_tags = {
+#     "Learn Tutorial" = "AFT"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
+#   change_management_parameters = {
+#     change_requested_by = "HashiCorp Learn"
+#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#   }
 
-  custom_fields = {
-    group = "non-prod"
-  }
+#   custom_fields = {
+#     group = "non-prod"
+#   }
 
-  account_customizations_name = "sandbox"
-}
+#   account_customizations_name = "sandbox"
+# }
